8b90e5c700fdc7c8346b3df68ed98eb018e5fa31
waip-wood <nicola.zordan94@gmail.com>
2025-04-11 15:48:39 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Gestionale WAIP</h1>
+      <p>Benvenuto! Il gestionale è online.</p>
+    </div>
+  );
+}
